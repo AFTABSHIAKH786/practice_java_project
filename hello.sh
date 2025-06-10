332068c994b202679a204285c5c3a1ced1592e41
Shaikh Aftab <2oe1bs36vcccidzowwmh961wyumpze@bots.bitbucket.org>
2025-06-10 10:48:17 +0530

    added java code

--- hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "checking scm"
-
-echo "verifying again"
-echo "scm using bitbucket webhook"
-echo "Hello the time is $(date)" >> /home/server/hello.txt
-echo "scm using bitbucket webhook"
